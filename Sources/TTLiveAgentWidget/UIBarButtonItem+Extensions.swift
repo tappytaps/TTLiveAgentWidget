ed24915f0dc14bf6a9ba53970ec878d1d12ed9a5
Lukáš Boura <lukas.boura@tappytaps.com>
2026-06-02 16:00:26 +0200

    Fix close button style

--- Sources/TTLiveAgentWidget/UIBarButtonItem+Extensions.swift
+++ Sources/TTLiveAgentWidget/UIBarButtonItem+Extensions.swift
@@ -4,9 +4,9 @@ extension UIBarButtonItem {
     
     static func closeItem(target: Any, action: Selector) -> UIBarButtonItem {
         if let closeImage = TTLiveAgentWidget.shared.images.close {
-            return UIBarButtonItem(image: closeImage, style: .done, target: target, action: action)
+            return UIBarButtonItem(image: closeImage, style: .plain, target: target, action: action)
         } else {
-            return UIBarButtonItem(title: "Close", style: .done, target: target, action: action)
+            return UIBarButtonItem(title: "Close", style: .plain, target: target, action: action)
         }
     }
     
